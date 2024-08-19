8d973b1fc617e39c661609c2d99c41a63308412f
manrubbra <manrubbra@protonmail.com>
2024-08-19 19:45:38 +0200

    ADD - Modified the Interface of task management

--- TaskTrackerCLI/TaskTrackerCLI.BLL/TaskLogic/ITaskBll.cs
+++ TaskTrackerCLI/TaskTrackerCLI.BLL/TaskLogic/ITaskBll.cs
@@ -1,20 +1,28 @@
-using TaskTrackerCLI.BLL.Entities;
+using Entities = TaskTrackerCLI.BLL.Entities;
 
 namespace TaskTrackerCLI.BLL.TaskLogic
 {
     public interface ITaskBll
     {
-        /*
-        
-    Add, Update, and Delete tasks
-    Mark a task as in progress or done
-    List all tasks
-    List all tasks that are done
-    List all tasks that are not done
-    List all tasks that are in progress
+        ///<summary>
+        /// Create a new task in the JSON file
+        ///</summary>
+        Entities.Task Add(string description);
 
-        */
+        ///<summary>
+        /// Update an existing task. It can change the description and the status
+        ///</summary>
+        Entities.Task Update(int taskId, string description, Entities.TaskStatus status);
 
+        ///<summary>
+        /// Delete the task from the JSON file
+        ///</summary>
+        Entities.Task Delete(int taskId);
+
+        ///<summary>
+        /// Retrieve from the JSON file these task which fit with the filter (status)
+        ///</summary>
+        IObservable<Entities.Task> Get(Entities.TaskStatus filter);
         
     }
 }
--- TaskTrackerCLI/TaskTrackerCLI.BLL/TaskLogic/TaskBll.cs
+++ TaskTrackerCLI/TaskTrackerCLI.BLL/TaskLogic/TaskBll.cs
@@ -1,12 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TaskTrackerCLI.BLL.TaskLogic
 {
     public class TaskBll : ITaskBll
     {
-        
+        public Entities.Task Add(string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entities.Task Delete(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<Entities.Task> Get(Entities.TaskStatus filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entities.Task Update(int taskId, string description, Entities.TaskStatus status)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
